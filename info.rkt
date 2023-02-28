cd44926cdc5ab9a105fa25f8a3d6bb633469f29e
Javier Vivanco <javier123mendoza@gmail.com>
2023-02-28 11:05:27 -0300

    fix: deps

--- info.rkt
+++ info.rkt
@@ -1,8 +1,6 @@
 #lang info
 (define collection "struct-persist")
-(define deps '("base" "buid" ))
-;(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-;(define scribblings '(("scribblings/struct-persist.scrbl" ())))
-;(define pkg-desc "Description Here")
+(define deps '("base" "buid" "rackunit"))
+
 (define version "1")
 (define pkg-authors '(javier-vivanco))
